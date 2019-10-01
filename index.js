d828380937b954a539d9290288c74701df689981
Jim <jimyyang129@gmail.com>
2019-10-01 14:49:36 -0700

    finished lab2

---
+++ index.js
@@ -0,0 +1,16 @@
+var Greeter = /** @class */ (function () {
+    function Greeter(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Greeter.prototype.showGreeting = function () {
+        var row2 = document.getElementById('r1c2');
+        row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + ',' + ' this is Lab # 2' + '.';
+    };
+    Greeter.prototype.resetGreeting = function () {
+        var row2 = document.getElementById('r1c2');
+        row2.innerHTML = 'r1c2';
+    };
+    return Greeter;
+}());
+var greeter = new Greeter('Jim', 'Yang');
---
+++ index.ts
@@ -0,0 +1,22 @@
+class Greeter {
+
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    showGreeting() {
+        const row2 = document.getElementById('r1c2');
+        row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + ',' + ' this is Lab Assignment #2' + '.'
+    }
+
+    resetGreeting () {
+        const row2 = document.getElementById('r1c2');
+        row2.innerHTML = 'r1c2';
+    }
+
+}
+
+const greeter = new Greeter('Jim', 'Yang');
